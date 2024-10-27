30cf452134be73e772376ac0de252fd615a60316
emrahelis <emrahelis@gmail.com>
2024-10-27 13:07:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+// Exe Dosyası içerisinde, daha önce yazdığımız DLL dosyasını , proje dizinine bir libs klasörü açıp
+// daha önce oluşturduğumuz DLL dosyasını içerisine kopyaladık!!
+
+// daha sonra termal ekranından dotnet build yazdık.!!
+// dll içerisindeki sınıfları kullanmak için, namespaceleri eklemek gerekmektedir!!
+
+//using DLL_KodKutuphanesi;
+//Ogrenci o = new Ogrenci();
+
+Ogrenci o = new Ogrenci();
